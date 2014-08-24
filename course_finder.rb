5e22c0c3b281254a602f1b27c01b8e3228fda030
Evan Lloyd <eulloyd@gmail.com>
2014-08-24 15:36:49 +0900

    Add more tests

--- course_finder.rb
+++ course_finder.rb
@@ -1,6 +1,19 @@
+require 'open-uri'
+require 'nokogiri'
+require 'set'
+
 class CourseFinder
-  attr_accessor :url
+  attr_accessor :url, :results
   def initialize
     @url = 'http://ocw.mit.edu/courses/electrical-engineering-and-computer-science/'
+    @results = Set.new
+  end
+
+  def all_courses
+    page = Nokogiri::HTML(open(self.url))
+    page.css('.preview').each do |link|
+      full_link = 'http://ocw.mit.edu'+"#{link['href']}"
+      self.results.add(full_link) 
+    end
   end
 end
--- course_finder_spec.rb
+++ course_finder_spec.rb
@@ -1,13 +1,26 @@
 require './course_finder'
 
 RSpec.describe CourseFinder do 
+
   #needs description
   it 'is a class' do
     @instance = CourseFinder.new
     expect(@instance.instance_of?(CourseFinder)).to eq true
   end
+
+  it 'has results ' do
+    expect(CourseFinder.new.results).to eq Set.new 
+  end
+
   it 'has a url' do
     @instance = CourseFinder.new
     expect(@instance.url).to_not be nil
+  end
+  describe '.all_courses' do
+    it 'adds links to .results' do
+      @finder = CourseFinder.new
+      @finder.all_courses
+      expect(@finder.results.size).to_not be 0
+    end 
   end
 end
